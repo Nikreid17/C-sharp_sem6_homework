755d96b22ff555192e3b8c38ac9697d8352f9e46
Nickolay <nik5te@yandex.ru>
2023-04-07 09:25:17 +0300

    сколько чисел ввёл пользователь

---
+++ task41/Program.cs
@@ -0,0 +1,6 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+System.Console.Write("Введите несколько чисел через пробел: ");
+int[] array = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+System.Console.WriteLine(array.Length);
